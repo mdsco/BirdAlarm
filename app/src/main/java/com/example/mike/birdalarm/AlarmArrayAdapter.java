5d532e8cd1ec90914d6812df43f9920875c41b8a
mdsco <scoboria@gmail.com>
2016-10-12 14:22:41 -0400

    New item added to alarm list based on alarm object created when time is selected using the time picker,

--- app/src/main/java/com/example/mike/birdalarm/AlarmArrayAdapter.java
+++ app/src/main/java/com/example/mike/birdalarm/AlarmArrayAdapter.java
@@ -2,19 +2,13 @@ package com.example.mike.birdalarm;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by mike on 10/11/16.
- */
 
 public class AlarmArrayAdapter extends ArrayAdapter<Alarm> {
 
@@ -34,22 +28,15 @@ public class AlarmArrayAdapter extends ArrayAdapter<Alarm> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        System.out.println("position: " + position + "   convertView: " + convertView + "   parent: " + parent);
-
-
-
         if( convertView == null ) {
-
-            System.out.println("---------------------Got here2  ");
             LayoutInflater inflater = ((Activity) context).getLayoutInflater();
             convertView = inflater.inflate(layoutResourceId, parent, false);
         }
 
-        System.out.println("---------------------Got here");
-
         Alarm alarmItem = alarmList.get(position);
 
         TextView alarmListItem = (TextView) convertView.findViewById(R.id.alarm_list_item);
+
         alarmListItem.setText("Alarm set for: "
                 + alarmItem.getHour() + ":"
                 + alarmItem.getMinute());
--- app/src/main/java/com/example/mike/birdalarm/AlarmListFragment.java
+++ app/src/main/java/com/example/mike/birdalarm/AlarmListFragment.java
@@ -5,20 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 
 public class AlarmListFragment extends ListFragment {
 
-//    ArrayList<String> items = new ArrayList<String>();
-      ArrayList<Alarm> alarmItems = new ArrayList<Alarm>();
-//    ArrayAdapter<String> adapter;
-      AlarmArrayAdapter adapter;
+    ArrayList<Alarm> alarmItems = new ArrayList<Alarm>();
+    AlarmArrayAdapter adapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 //        items = new ArrayList<String>();
@@ -35,12 +32,7 @@ public class AlarmListFragment extends ListFragment {
 
         View view = inflater.inflate(R.layout.list_fragment, null);
 
-//        adapter = new ArrayAdapter<String>(
-//                getActivity(), R.layout.item, items);
-
-
-
-        adapter = new AlarmArrayAdapter(getActivity(), R.layout.list_fragment, alarmItems);
+        adapter = new AlarmArrayAdapter(getActivity(), R.layout.item, alarmItems);
 
         setListAdapter(adapter);
 
@@ -49,19 +41,17 @@ public class AlarmListFragment extends ListFragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState){
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
     }
 
-    public void addAlarm(String hour, String minute){
+    public void addAlarm(String hour, String minute) {
 
         Alarm alarm = new Alarm(new Integer(hour), new Integer(minute));
         alarmItems.add(alarm);
-//        adapter = new ArrayAdapter<String>(
-//                getActivity(), R.layout.item, items);
 
-        adapter = new AlarmArrayAdapter(getActivity(), R.layout.list_fragment, alarmItems);
+        adapter = new AlarmArrayAdapter(getActivity(), R.layout.item, alarmItems);
 
         setListAdapter(adapter);
 
