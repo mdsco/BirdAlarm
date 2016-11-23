02a7fb9c4ad198ccb3ae93757ec5d35390ad4f42
mike <scoboria@gmail.com>
2016-11-23 16:05:05 -0500

    Added alarm ring tone to be triggered when a notification popup occurs.

---
+++ app/src/main/java/com/example/mike/birdalarm/AlarmLockScreen.java
@@ -0,0 +1,21 @@
+package com.example.mike.birdalarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by mike on 11/22/16.
+ */
+
+public class AlarmLockScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.alarm_lock_screen);
+
+
+
+    }
+}
