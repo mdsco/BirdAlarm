d2716e87fb7b478bc00b441e43ca71b0a50de20d
mike <scoboria@gmail.com>
2017-01-03 00:57:42 -0500

    Created defaults for alarm type and alarm type name and set relevant places.  (With the actual Default class this time)

---
+++ app/src/main/java/com/example/mike/birdalarm/Defaults.java
@@ -0,0 +1,9 @@
+package com.example.mike.birdalarm;
+
+
+public class Defaults {
+
+    public static final String DEFAULT_ALARM_TYPE_NAME = "cute robin chirping";
+    public static final String DEFAULT_ALARM_TYPE = "cute_robin_chirping.mp4";
+
+}
